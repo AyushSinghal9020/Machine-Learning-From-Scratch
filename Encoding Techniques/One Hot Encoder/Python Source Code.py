08d9be9caa1c27dcd65e3280918789870652986a
Ayush Singhal <91676003+AyushSinghal9020@users.noreply.github.com>
2023-04-12 18:26:29 +0530

    Update Python Source Code.py
    Adding Functionality - Iterating Over the Columns

--- Encoding Techniques/One Hot Encoder/Python Source Code.py
+++ Encoding Techniques/One Hot Encoder/Python Source Code.py
@@ -1,4 +1,8 @@
-for i in sample_data.value_counts().index:
-    sample_data["MSZoning" + "_" + i[0]] = np.where(sample_data["MSZoning"] == i[0] , 1 , 0)
-    
-sample_data.drop("MSZoning" + "_" + str(sample_data["MSZoning"].value_counts().index[0]) , axis = 1 , inplace = True)
+def sample_func(dataframe , columns):
+    for i in columns:
+                
+        for j in dataframe[i].value_counts().index[0]:
+                
+            dataframe[i + "_" + j[0]] = np.where(dataframe[i] == j[0] , 1 , 0)
+                
+        dataframe.drop(i , axis = 1 , inplace = True)
