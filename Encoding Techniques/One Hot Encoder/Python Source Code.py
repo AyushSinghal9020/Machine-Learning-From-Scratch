6147f955b50bdc3181f3094da02749098ea66413
Ayush Singhal <91676003+AyushSinghal9020@users.noreply.github.com>
2023-04-12 18:27:53 +0530

    Update Python Source Code.py
    Added Functionality - `Drop`

--- Encoding Techniques/One Hot Encoder/Python Source Code.py
+++ Encoding Techniques/One Hot Encoder/Python Source Code.py
@@ -1,14 +1,17 @@
-def sample_func(dataframe , columns):
+def sample_func(dataframe , columns , drop = None):
     
     if len(columns) == 1 :
         
-        for i in columns:
-                
-            for j in dataframe[i].value_counts().index[0]:
-                
-                dataframe[i + "_" + j[0]] = np.where(dataframe[i] == j[0] , 1 , 0)
-                
-            dataframe.drop(i , axis = 1 , inplace = True)
+        for j in dataframe[columne[0]].value_counts().index[0]:
+
+            dataframe[columns[0] + "_" + j[0]] = np.where(dataframe[columns[0]] == j[0] 
+                                                          , 1 , 0)
+        if drop == "first" :
+            
+            dataframe.drop(str(columns[0]) + "_" + sample_data[columne[0]].value_counts.index[0] , 
+                          axis = 1 , inplace = True)
+
+        dataframe.drop(columns[0] , axis = 1 , inplace = True)
 
     else : 
         
@@ -17,5 +20,9 @@ def sample_func(dataframe , columns):
             for j in dataframe[i].value_counts().index[0]:
                 
                 dataframe[i + "_" + j] = np.where(dataframe[i] == j , 1 , 0)
-                
+            if drop == "first" :
+            
+                dataframe.drop(str(i) + "_" + sample_data[i].value_counts.index[0] , 
+                               axis = 1 , inplace = True)
+            
             dataframe.drop(i , axis = 1 , inplace = True)
