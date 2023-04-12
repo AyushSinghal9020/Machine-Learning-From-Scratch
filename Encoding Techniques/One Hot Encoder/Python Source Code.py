96d1eb4f68262f18b7f40eda80441633bea51168
Ayush Singhal <91676003+AyushSinghal9020@users.noreply.github.com>
2023-04-12 18:27:19 +0530

    Update Python Source Code.py
    Edge Case Detection

--- Encoding Techniques/One Hot Encoder/Python Source Code.py
+++ Encoding Techniques/One Hot Encoder/Python Source Code.py
@@ -1,8 +1,21 @@
 def sample_func(dataframe , columns):
-    for i in columns:
+    
+    if len(columns) == 1 :
+        
+        for i in columns:
                 
-        for j in dataframe[i].value_counts().index[0]:
+            for j in dataframe[i].value_counts().index[0]:
                 
-            dataframe[i + "_" + j[0]] = np.where(dataframe[i] == j[0] , 1 , 0)
+                dataframe[i + "_" + j[0]] = np.where(dataframe[i] == j[0] , 1 , 0)
                 
-        dataframe.drop(i , axis = 1 , inplace = True)
+            dataframe.drop(i , axis = 1 , inplace = True)
+
+    else : 
+        
+        for i in columns:
+                
+            for j in dataframe[i].value_counts().index[0]:
+                
+                dataframe[i + "_" + j] = np.where(dataframe[i] == j , 1 , 0)
+                
+            dataframe.drop(i , axis = 1 , inplace = True)
