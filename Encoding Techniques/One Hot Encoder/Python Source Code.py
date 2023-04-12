799f2ae6f59ef13ac66519f592e1582f93d3bb43
Ayush Singhal <91676003+AyushSinghal9020@users.noreply.github.com>
2023-04-12 18:28:49 +0530

    Update Python Source Code.py
    Added Functionality - `Dtype`

--- Encoding Techniques/One Hot Encoder/Python Source Code.py
+++ Encoding Techniques/One Hot Encoder/Python Source Code.py
@@ -1,28 +1,72 @@
-def sample_func(dataframe , columns , drop = None):
+def sample_func(dataframe , columns , min_frequency = None , dtype = float):
+
+    if not min_frequency == None:
     
-    if len(columns) == 1 :
-        
-        for j in dataframe[columne[0]].value_counts().index[0]:
-
-            dataframe[columns[0] + "_" + j[0]] = np.where(dataframe[columns[0]] == j[0] 
-                                                          , 1 , 0)
-        if drop == "first" :
-            
-            dataframe.drop(str(columns[0]) + "_" + sample_data[columne[0]].value_counts.index[0] , 
-                          axis = 1 , inplace = True)
-
-        dataframe.drop(columns[0] , axis = 1 , inplace = True)
-
-    else : 
-        
-        for i in columns:
+        if len(columns) == 1 :
                 
-            for j in dataframe[i].value_counts().index[0]:
-                
-                dataframe[i + "_" + j] = np.where(dataframe[i] == j , 1 , 0)
-            if drop == "first" :
-            
-                dataframe.drop(str(i) + "_" + sample_data[i].value_counts.index[0] , 
-                               axis = 1 , inplace = True)
-            
+            inf = [j 
+                for j in dataframe[columne[0]].value_counts().index 
+                if dataframe[columne[0]].value_counts()[j] > min_frequency]
+        
+            for j in dataframe[columne[0]].value_counts().index:
+        
+                if not j in inf:
+        
+                    dataframe[columne[0] + "_" + j[0]] = np.where(dataframe[columne[0]] == j[0] , 
+                                                                  dtype(1) , dtype(0))
+        
+                else: 
+        
+                    dataframe[columne[0] + "_other"] = np.where(dataframe[columne[0]].isin(inf) , 
+                                                                dtype(1) , dtype(0))
+                if drop == "first" or drop == "if_binary":
+                    dataframe.drop(str(columns[0]) + "_" + sample_data[columne[0]].value_counts.index[0] , 
+                                   axis = 1 , inplace = True)
             dataframe.drop(i , axis = 1 , inplace = True)
+        else :
+            for i in columns:
+                
+                inf = [j 
+                    for j in dataframe[i].value_counts().index 
+                    if dataframe[i].value_counts()[j] > min_frequency]
+            
+                for j in dataframe[i].value_counts().index:
+            
+                    if not j in inf:
+            
+                        dataframe[i + "_" + j] = np.where(dataframe[i] == j , 
+                                                          dtype(1) , dtype(0))
+
+                    else: 
+            
+                        dataframe[i + "_other"] = np.where(dataframe[i].isin(inf) , 
+                                                           dtype(1) , dtype(0))
+                    if drop == "first" or drop == "if_binary" :
+                        dataframe.drop(str(i) + "_" + sample_data[i].value_counts.index[0] , 
+                                       axis = 1 , inplace = True)
+                dataframe.drop(i , axis = 1 , inplace = True) 
+    else :    
+        
+        if len(columns) == 1:
+        
+            for j in dataframe[columne[0]].value_counts().index[0]:
+                
+                dataframe[columne[0] + "_" + j[0]] = np.where(dataframe[columne[0]] == j[0] , 
+                                                        dtype(1) , dtype(0))
+            if drop == "first" or drop == "if_binary" :
+                        dataframe.drop(str(i) + "_" + sample_data[i].value_counts.index[0] , 
+                                       axis = 1 , inplace = True)
+            dataframe.drop(columne[0] , axis = 1 , inplace = True)
+
+        else : 
+        
+            for i in columns:
+                    
+                for j in dataframe[i].value_counts().index[0]:
+                    
+                    dataframe[i + "_" + j] = np.where(dataframe[i] == j , 
+                                                      dtype(1) , dtype(0))
+                if drop == "first" or drop == "if_binary" :
+                        dataframe.drop(str(i) + "_" + sample_data[i].value_counts.index[0] , 
+                                       axis = 1 , inplace = True)
+                dataframe.drop(i , axis = 1 , inplace = True)
