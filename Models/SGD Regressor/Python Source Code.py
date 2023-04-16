04257f17e2acfa41b928bf6030503a22405c6610
Ayush Singhal <91676003+AyushSinghal9020@users.noreply.github.com>
2023-04-16 21:42:48 +0530

    Update Python Source Code.py

--- Models/SGD Regressor/Python Source Code.py
+++ Models/SGD Regressor/Python Source Code.py
@@ -1,17 +1,24 @@
 squared_mean = lambda predictions , actuals : np.sum((predictions - actual) ** 2)
 
 def huber_loss(prediction , actuals , delta):
+    
     loss = 0
+    
     for pred , act in zip(predictions , actuals):
+    
         if abs(pred - act) <= delta:
+        
             loss += (1 / 2) * pred - act
+        
         else :
+        
             loss += (delta * abs(pred - act)) - ((1 / 2) * (delta ** 2))
+    
     return loss
 
 epsilon_intensive = lambda predictions , actuals , epsilon : np.where((predictions - actuals) < epsilon , 0 , (predictions - actuals))
 
-def SGDRegressor(X , y , loss_type = "sqaured_mean" , delta = 0.2 , epsilon = 0):
+def SGDRegressor(X , y , loss_type = "sqaured_mean" , delta = 0.2 , epsilon = 0 , alpha = 0.0001):
     
     weights = abs(np.random.randn(X.shape[0]))
     biases = abs(np.random.randn(1))
@@ -22,15 +29,21 @@ def SGDRegressor(X , y , loss_type = "sqaured_mean" , delta = 0.2 , epsilon = 0)
     for _ in range(300):
     
         pred = weights * features + biases
+    
         if loss_type == "sqaured_mean":
+        
             loss = squared_mean(pred , y , delta)
+        
         elif loss_type == "huber" : 
+            
             loss = huber(pred , y)
+        
         else :
+        
             loss = epsilon_intensie(pred , y , epsilon)
         losses.append(loss)
         
-        weights -= -2 * loss * 0.01
-        biases -= -2 * 30 * loss * 0.01
+        weights -= -2 * loss * alpha
+        biases -= -2 * 30 * loss * alpha
 
     return weights , biases
