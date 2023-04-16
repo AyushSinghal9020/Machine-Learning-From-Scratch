e413731cea4a6ae9d20472acea81a6608d837b34
Ayush Singhal <91676003+AyushSinghal9020@users.noreply.github.com>
2023-04-16 21:41:09 +0530

    Update Python Source Code.py

--- Models/SGD Regressor/Python Source Code.py
+++ Models/SGD Regressor/Python Source Code.py
@@ -1,12 +1,18 @@
-def SGDRegressor():
-    weights = abs(np.random.randn(1))
+def SGDRegressor(X , y):
+    
+    weights = abs(np.random.randn(X.shape[0]))
     biases = abs(np.random.randn(1))
+    
     predic = []
     losses = []
+    
     for _ in range(300):
+    
         pred = weights * features + biases
-        loss = np.sum((pred - target) ** 2)
+        
+        loss = np.sum((pred - y) ** 2)
         losses.append(loss)
+        
         weights -= -2 * loss * 0.01
         biases -= -2 * 30 * loss * 0.01
 
