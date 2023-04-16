36dd3872fabde8a6cbf9772f09cc5784b3a3f35d
Ayush Singhal <91676003+AyushSinghal9020@users.noreply.github.com>
2023-04-16 21:41:48 +0530

    Update Python Source Code.py

--- Models/SGD Regressor/Python Source Code.py
+++ Models/SGD Regressor/Python Source Code.py
@@ -1,6 +1,15 @@
 squared_mean = lambda predictions , actuals : np.sum((predictions - actual) ** 2)
 
-def SGDRegressor(X , y):
+def huber_loss(prediction , actuals):
+    loss = 0
+    for pred , act in zip(predictions , actuals):
+        if abs(pred - act) <= 0.2:
+            loss += (1 / 2) * pred - act
+        else :
+            loss += (0.2 * abs(pred - act)) - ((1 / 2) * (0.2 ** 2))
+    return loss
+
+def SGDRegressor(X , y , loss = "sqaured_mean"):
     
     weights = abs(np.random.randn(X.shape[0]))
     biases = abs(np.random.randn(1))
@@ -11,8 +20,10 @@ def SGDRegressor(X , y):
     for _ in range(300):
     
         pred = weights * features + biases
-        
-        loss = squared_mean(pred , y)
+        if loss == "sqaured_mean":
+            loss = squared_mean(pred , y)
+        else : 
+            loss = huber(pred , y)
         losses.append(loss)
         
         weights -= -2 * loss * 0.01
