e210e3b13d599a62e3997a4e1d18e418e3973ae1
Ayush Singhal <91676003+AyushSinghal9020@users.noreply.github.com>
2023-04-17 16:09:09 +0530

    Update Python Source Code.py

--- Models/SGD Regressor/Python Source Code.py
+++ Models/SGD Regressor/Python Source Code.py
@@ -2,73 +2,90 @@
 
 import numpy as np 
 
-squared_mean = lambda predictions , actuals : np.sum((predictions - actual) ** 2)
-
-def huber_loss(prediction , actuals , delta):
+class SGDRegressor:
     
-    loss = 0
-    
-    for pred , act in zip(predictions , actuals):
-    
-        if abs(pred - act) <= delta:
-        
-            loss += (1 / 2) * pred - act
-        
-        else :
-        
-            loss += (delta * abs(pred - act)) - ((1 / 2) * (delta ** 2))
-    
-    return loss
-
-epsilon_intensive = lambda predictions , actuals , epsilon : np.where((predictions - actuals) < epsilon , 0 , (predictions - actuals))
-
-def SGDRegressor(X , y , 
-                 loss_type = "sqaured_mean" , delta = 0.2 , 
+    def __init__(self , loss_type = "sqaured_mean" , delta = 0.2 , 
                  epsilon = 0 , alpha = 0.0001 , 
                 fit_intercept = True , max_iter = 1000 , 
                 shuffle = False , early_stopping = False):
-    
-    weights = np.zeors(shape = X.shape[0])
-    biases = np.zeors(1)
-    
-    predic = []
-    losses = []
-    
-    for epochs in range(max_iter):
         
-        if shuffle :
-        
-            X = np.random.shuffle(x)
+        pass
     
-        pred = weights * X + biases
+    def fit(self , X , y , sample_weight = None): 
     
-        if loss_type == "sqaured_mean":
-        
-            loss = squared_mean(pred , y , delta)
-        
-        elif loss_type == "huber" : 
+        if sample_weight != None:
             
-            loss = huber(pred , y)
-        
-        else :
-        
-            loss = epsilon_intensie(pred , y , epsilon)
-        
-        losses.append(loss)
-        
-        if early_stopping: 
+            if len(sampe_weight) != (X.shape[0] + 1):
             
-            if losses[epochs] == losses[epochs - 1]:
+                warnings.warn("Please use the correct shape of `sample_weights`. using `0` as the parameters!!")
+            
+            else : 
                 
-                break
+                weights , biases = sample_weight[:1] , sample_weight[0]
         
-        weights -= -2 * loss * alpha
+        else : 
         
-        if fit_intercept:
+            weights = np.zeors(shape = X.shape[0])
+            biases = np.zeors(1)
             
-            biases -= -2 * 30 * loss * alpha
+        predic = []
+        losses = []
+        
+        for epochs in range(max_iter):
+            
+            if shuffle :
+            
+                X = np.random.shuffle(x)
+        
+            pred = weights * X + biases
+        
+            if loss_type == "sqaured_mean":
+            
+                loss = squared_mean(pred , y , delta)
+            
+            elif loss_type == "huber" : 
+                
+                loss = huber(pred , y)
+            
+            else :
+            
+                loss = epsilon_intensie(pred , y , epsilon)
+            
+            losses.append(loss)
+            
+            if early_stopping: 
+            
+                if losses[epochs] == losses[epochs - 1]:
+            
+                    break
+            
+            weights -= -2 * loss * alpha
+            
+            if fit_intercept:
+            
+                biases -= -2 * 30 * loss * alpha
 
-    return weights , biases
+        return weights , biases
+
+    squared_mean = lambda predictions , actuals : np.sum((predictions - actual) ** 2)
+
+    epsilon_intensive = lambda predictions , actuals , epsilon : np.where((predictions - actuals) < epsilon , 0 , (predictions - actuals))
+    
+    def huber_loss(prediction , actuals , delta):
+        
+        loss = 0
+        
+        for pred , act in zip(predictions , actuals):
+        
+            if abs(pred - act) <= delta:
+            
+                loss += (1 / 2) * pred - act
+            
+            else :
+            
+                loss += (delta * abs(pred - act)) - ((1 / 2) * (delta ** 2))
+        
+        return loss
 
 # From Scratch Using Pytorch
 
