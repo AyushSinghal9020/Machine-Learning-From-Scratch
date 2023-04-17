63d51f20b9b4431b9cdb33d8b2ebda726110838c
Ayush Singhal <91676003+AyushSinghal9020@users.noreply.github.com>
2023-04-17 16:17:56 +0530

    Update Python Source Code.py

--- Models/Ridge Regression/Python Source Code.py
+++ Models/Ridge Regression/Python Source Code.py
@@ -1,12 +1,18 @@
-def RidgeRegression():
-    weights = abs(np.random.randn(1))
-    biases = abs(np.random.randn(1))
+def RidgeRegression(X , y):
+    
+    weights = np.random.randn(X.shape[0])
+    biases = np.random.randn(1)
+    
     predic = []
     losses = []
+    
     for _ in range(300):
+    
         pred = weights * features + biases
+        
         loss = np.sum((60 - (weights * 30 + biases)) + (weights ** 2))
         losses.append(loss)
+        
         weights -= ((-2* loss) + (2 * weights)) * 0.01
         biases -= -2 * 30 * loss * 0.01
 
