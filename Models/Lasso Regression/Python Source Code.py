378530f1b805ff8a8b92fdb2dd4763af7e0465cd
Ayush Singhal <91676003+AyushSinghal9020@users.noreply.github.com>
2023-04-17 16:27:33 +0530

    Update Python Source Code.py

--- Models/Lasso Regression/Python Source Code.py
+++ Models/Lasso Regression/Python Source Code.py
@@ -1,23 +1,39 @@
-def LassoRegression(X , y , 
-                    alpha = 0.1 , fit_intercepts = True):
+class LassoRegression:
     
-    weights = np.zeros(shape = X.shape[0])
-    biases = np.zeros(shape = (1))
+    def __init__(self , X , y , 
+                    alpha = 0.1 , fit_intercept = True):pass
     
-    predic = []
-    losses = []
-    
-    for _ in range(300):
-    
-        pred = weights * features + biases
+    def fit(self , X , y , sample_weight): 
         
-        loss = np.sum((y - (weights * X + biases)) + (alpha * weights))
-        losses.append(loss)
+        if sample_weight != None:
+            
+            if len(sampe_weight) != (X.shape[0] + 1):
+            
+                warnings.warn("Please use the correct shape of `sample_weights`. using `0` as the parameters!!")
+            
+            else : 
+                
+                weights , biases = sample_weight[:1] , sample_weight[0]
         
-        weights -= ((-2* loss) + alpha) * 0.01
+        else : 
         
-        if fit_intercept:
-                      
-            biases -= -2 * loss * 0.01
+            weights = np.zeors(shape = X.shape[0])
+            biases = np.zeors(1)
+    
+        predic = []
+        losses = []
 
-    return weights , biases
+        for _ in range(300):
+
+            pred = weights * features + biases
+
+            loss = np.sum((y - (weights * X + biases)) + (alpha * weights))
+            losses.append(loss)
+
+            weights -= ((-2* loss)) + alpha
+
+            if fit_intercept:
+
+                biases -= -2 * loss * 0.01
+
+        return weights , biases
