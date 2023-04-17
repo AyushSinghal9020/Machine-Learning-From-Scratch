73f0773dba25de00d85aee6045c46f1b4ccb143c
Ayush Singhal <91676003+AyushSinghal9020@users.noreply.github.com>
2023-04-17 16:27:01 +0530

    Update Python Source Code.py

--- Models/Lasso Regression/Python Source Code.py
+++ Models/Lasso Regression/Python Source Code.py
@@ -1,13 +1,19 @@
-def LassoRegression():
-    weights = abs(np.random.randn(1))
-    biases = abs(np.random.randn(1))
+def LassoRegression(X , y):
+    
+    weights = np.random.randn(X.shape[0])
+    biases = np.random.randn(1)
+    
     predic = []
     losses = []
+    
     for _ in range(300):
+    
         pred = weights * features + biases
-        loss = np.sum((60 - (weights * 30 + biases)) + (weights))
+        
+        loss = np.sum((y - (weights * X + biases)) + (weights))
         losses.append(loss)
+        
         weights -= ((-2* loss) + 1) * 0.01
-        biases -= -2 * 30 * loss * 0.01
+        biases -= -2 * loss * 0.01
 
     return weights , biases
