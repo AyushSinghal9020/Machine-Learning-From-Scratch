6c16da8f9d5965d8e9a09d0d0322a94eb6d5d21a
Ayush Singhal <91676003+AyushSinghal9020@users.noreply.github.com>
2023-04-17 16:15:21 +0530

    Update Python Source Code.py

--- Models/Ridge Regression/Python Source Code.py
+++ Models/Ridge Regression/Python Source Code.py
@@ -1,2 +1,8 @@
 features = np.array([x for x in range(0 , 200 , 1)])
-target = np.array([x + 1 for x in range(0 , 400 , 2)])
+target = []
+for i in range(200):
+    if i < 26:
+        target.append(i**(1 / 2))
+    else :
+        target.append(i**(1/3))
+target = np.array(target)
