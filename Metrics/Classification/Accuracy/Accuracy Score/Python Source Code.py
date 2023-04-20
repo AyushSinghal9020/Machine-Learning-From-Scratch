a8cf4e07f44867e0ba249c5f5056c701da87f45b
Ayush Singhal <91676003+AyushSinghal9020@users.noreply.github.com>
2023-04-20 18:27:34 +0530

    Create Python Source Code.py

---
+++ Metrics/Classification/Accuracy/Accuracy Score/Python Source Code.py
@@ -0,0 +1,13 @@
+def accuracy_score(actuals , predictions):
+    
+    correct_ones = 0
+    
+    for actual  , predicted in zip(actuals , predictions):
+        
+        if predicted == actual:
+            
+            correct_ones += 1
+
+    asc = correct_ones / len(predictions)
+
+    return asc
