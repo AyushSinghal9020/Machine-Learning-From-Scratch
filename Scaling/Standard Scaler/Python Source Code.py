51fe39778bdd62dcd126d72327643ed2efd9c3ae
Ayush Singhal <91676003+AyushSinghal9020@users.noreply.github.com>
2023-04-23 08:21:55 +0530

    Update Python Source Code.py

--- Scaling/Standard Scaler/Python Source Code.py
+++ Scaling/Standard Scaler/Python Source Code.py
@@ -1,10 +1,18 @@
 def standard_scaler(array , copy = True):
     if copy:
+        
         y = np.empty(shape = array.shape)
+        
         for i,j in enumerate(array):
+            
             y[i] = (j - array.mean()) / (array.std())
+        
         return y
+    
     else :
+        
         for i,j in enumerate(array):
+            
             array[i] = (j - array.mean()) / (array.std())
+        
         return array
