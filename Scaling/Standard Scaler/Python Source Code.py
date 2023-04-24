39abc0482016f3c33cd7d12d41020208c74f365e
Ayush Singhal <91676003+AyushSinghal9020@users.noreply.github.com>
2023-04-24 15:39:14 +0530

    Update Python Source Code.py

--- Scaling/Standard Scaler/Python Source Code.py
+++ Scaling/Standard Scaler/Python Source Code.py
@@ -1,11 +1,12 @@
-def standard_scaler(array , copy = True):
+def min_max_scaler(array , copy = True):
+    
     if copy:
         
         y = np.empty(shape = array.shape)
         
         for i,j in enumerate(array):
             
-            y[i] = (j - array.mean()) / (array.std())
+            y[i] = (j - array.min()) / (y.max() - y.min())
         
         return y
     
@@ -13,6 +14,6 @@ def standard_scaler(array , copy = True):
         
         for i,j in enumerate(array):
             
-            array[i] = (j - array.mean()) / (array.std())
+            array[i] = (j - array.min()) / (array.max() - array.min())
         
         return array
