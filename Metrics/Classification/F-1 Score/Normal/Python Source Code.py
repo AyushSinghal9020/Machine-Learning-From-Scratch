d7282de286b1291c83b48d06228d3b931bc5cdab
Ayush Singhal <91676003+AyushSinghal9020@users.noreply.github.com>
2023-04-24 20:30:11 +0530

    Update Python Source Code.py

--- Metrics/Classification/F-1 Score/Normal/Python Source Code.py
+++ Metrics/Classification/F-1 Score/Normal/Python Source Code.py
@@ -1,21 +1,72 @@
-def accuracy(actuals , predictions):
+def f1_score(actuals , predicitions):
     
-    k_labels = []
+    def balanced_accuracy_score(predictions , actuals):
+
+        metrics = np.empty(shape = (2 , len(np.unique(pred)) + 1))
+
+        for uniques in range(len(np.unique(pred))):
+
+            x = np.empty(shape = (3))
+
+            trues = 0
+            falses = 0
+
+            for actu , predi in zip(act , pred):
+
+                if ((actu == np.unique(pred)[uniques]) and (predi == np.unique(pred)[uniques])):
+
+                    trues += 1
+
+                else :
+
+                    falses += 1
+
+            x[0] = uniques
+            x[1] = trues
+            x[2] = falses
+
+            metrics[i] = x
+
+        recall = []
+
+        if metrics.shape[0] == 2:
+
+            recall.append(metrics[0][1] / (metrics[0][1] + metrics[1][2]))
+            recall.append(metrics[1][1] / (metrics[1][1] + metrics[0][2]))
+
+        else :
+
+            for labels in range(len(metrics)): 
+
+                x = [j[2] 
+                     for j in metrics[:labels]+metrics[labels+1:]]
+
+                recall.append(metrics[labels][1] / np.sum(np.array(x)))
+
+        return np.sum(np.array(recall))
     
-    for label in np.unique(pred):
-        
-        correct_ones = 0
+    def precision(actuals , predictions):
 
-        for actual  , predicted in zip(actuals , predictions):
+        k_labels = []
 
-            if predicted == label and actual == label:
+        for label in np.unique(pred):
 
-                correct_ones += 1
+            correct_ones = 0
 
-        asc = correct_ones / len(predictions)
-        
-        k_labels.append(asc)
+            for actual  , predicted in zip(actuals , predictions):
 
-    k_labels = np.array(k_labels)
+                if predicted == label and actual == label:
+
+                    correct_ones += 1
+
+            asc = correct_ones / len(predictions)
+
+            k_labels.append(asc)
+
+        k_labels = np.array(k_labels)
+
+        return k_labels
     
-    return k_labels
+    f1_score = 1 / (1/recall(actuals , predictions)) + (1 / precision(actuals , predictions))
+    
+    return f1_score
