a430ffd841a97c19f87e1b70fadec2bc3e12e8dc
Ayush Singhal <91676003+AyushSinghal9020@users.noreply.github.com>
2023-04-24 20:29:55 +0530

    Update Python Source Code.py

--- Metrics/Classification/F-1 Score/Normal/Python Source Code.py
+++ Metrics/Classification/F-1 Score/Normal/Python Source Code.py
@@ -1,44 +1,21 @@
-def balanced_accuracy_score(predictions , actuals):
+def accuracy(actuals , predictions):
+    
+    k_labels = []
+    
+    for label in np.unique(pred):
+        
+        correct_ones = 0
 
-    metrics = np.empty(shape = (2 , len(np.unique(pred)) + 1))
-    
-    for uniques in range(len(np.unique(pred))):
+        for actual  , predicted in zip(actuals , predictions):
+
+            if predicted == label and actual == label:
+
+                correct_ones += 1
+
+        asc = correct_ones / len(predictions)
         
-        x = np.empty(shape = (3))
-        
-        trues = 0
-        falses = 0
-        
-        for actu , predi in zip(act , pred):
-            
-            if ((actu == np.unique(pred)[uniques]) and (predi == np.unique(pred)[uniques])):
-            
-                trues += 1
-            
-            else :
-            
-                falses += 1
-        
-        x[0] = uniques
-        x[1] = trues
-        x[2] = falses
-        
-        metrics[i] = x
+        k_labels.append(asc)
+
+    k_labels = np.array(k_labels)
     
-    recall = []
-    
-    if metrics.shape[0] == 2:
-    
-        recall.append(metrics[0][1] / (metrics[0][1] + metrics[1][2]))
-        recall.append(metrics[1][1] / (metrics[1][1] + metrics[0][2]))
-    
-    else :
-    
-        for labels in range(len(metrics)): 
-            
-            x = [j[2] 
-                 for j in metrics[:labels]+metrics[labels+1:]]
-            
-            recall.append(metrics[labels][1] / np.sum(np.array(x)))
-    
-    return np.sum(np.array(recall))
+    return k_labels
