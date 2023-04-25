ccfa3ea71a8c3e45e7e9ea1a5d91c6a663be0497
Ayush Singhal <91676003+AyushSinghal9020@users.noreply.github.com>
2023-04-25 05:57:38 +0530

    Create Python Source Code.py

---
+++ Metrics/Classification/Precision Score/Precision /Python Source Code.py
@@ -0,0 +1,25 @@
+def top_k_accuracy(actuals , predictions , k):
+    
+    k_labels = []
+    
+    for label in np.unique(pred):
+        
+        correct_ones = 0
+
+        for actual  , predicted in zip(actuals , predictions):
+
+            if predicted == label and actual == label:
+
+                correct_ones += 1
+            # Else condition here 
+            else :
+                
+                false_ones += 1
+        
+        asc = correct_ones / false_ones
+        
+        k_labels.append(asc)
+
+    k_labels = np.array(k_labels)
+    
+    return K_lables    
