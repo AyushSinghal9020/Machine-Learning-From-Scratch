7010c68e9511d39728e925e93360d9564521eba0
Ayush Singhal <91676003+AyushSinghal9020@users.noreply.github.com>
2023-05-04 18:28:56 +0530

    Update Python Source Code.py

--- Models/Decision Trees/Python Source Code.py
+++ Models/Decision Trees/Python Source Code.py
@@ -18,7 +18,27 @@ class DecisionTree:
         self.n_features=n_features
         self.root=None
         
-    def fit(self , X , y):pass
+    def fit(self , X , y):
+        self.n_features = X.shape[1] if not self.n_features else min(X.shape[1],self.n_features)
+        self.root = self._grow_tree(X, y)
+    
+    def _grow_tree(self, X, y, depth=0):
+        
+        n_samples, n_feats = X.shape
+        n_labels = len(np.unique(y))
+
+        if (depth>=self.max_depth or n_labels==1 or n_samples<self.min_samples_split):
+            leaf_value = self._most_common_label(y)
+            return Node(value=leaf_value)
+
+        feat_idxs = np.random.choice(n_feats, self.n_features, replace=False)
+
+        best_feature, best_thresh = self._best_split(X, y, feat_idxs)
+
+        left_idxs, right_idxs = self._split(X[:, best_feature], best_thresh)
+        left = self._grow_tree(X[left_idxs, :], y[left_idxs], depth+1)
+        right = self._grow_tree(X[right_idxs, :], y[right_idxs], depth+1)
+        return Node(best_feature, best_thresh, left, right)
     
     def _best_split(self, X, y, feat_idxs):
         
