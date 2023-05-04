b9bd916799f49a9cda3292955d5f6715f87a1028
Ayush Singhal <91676003+AyushSinghal9020@users.noreply.github.com>
2023-05-04 18:26:09 +0530

    Create Python Source Code.py

---
+++ Models/Decision Trees/Python Source Code.py
@@ -0,0 +1,10 @@
+class Node:
+    def __init__(self, feature=None, threshold=None, left=None, right=None,*,value=None):
+        self.feature = feature
+        self.threshold = threshold
+        self.left = left
+        self.right = right
+        self.value = value
+        
+    def is_leaf_node(self):
+        return self.value is not None
